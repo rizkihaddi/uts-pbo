a29792a59539808dbfc67f7168045e5f79bc13e6
rizkihaddi <115798470+rizkihaddi@users.noreply.github.com>
2022-11-19 17:50:41 +0700

    Add files via upload

---
+++ bangun_ruang/Bola.java
@@ -0,0 +1,63 @@
+package bangun_ruang;
+
+public class Bola extends implemen {
+    private double jari2;
+
+    public Bola() {
+        setJari2(1);
+    }
+
+    public Bola(int jari2) {
+        setJari2(jari2);
+    }
+
+    @Override
+    public void setTinggi(double Tinggi) {
+        this.Tinggi = (int) (Tinggi < 0 ? 1 : Tinggi);
+    }
+
+    @Override
+    public void setPanjang(int Panjang) {
+        this.Panjang = Panjang < 0 ? 1 : Panjang;
+    }
+
+    @Override
+    public void setLebar(int Lebar) {
+        this.Lebar = Lebar < 0 ? 1 : Lebar;
+    }
+
+    @Override
+    public double getVolume() {
+        return ((4 * 3.14 * Math.pow(jari2, 3)) / 3);
+    }
+
+    @Override
+    public double getLuas() {
+        return 4 * 3.14 * Math.pow(jari2, 2);
+    }
+
+    @Override
+    public void setJari2(double jari2) {
+        this.jari2 = jari2 < 0 ? 1 : jari2;
+    }
+
+    @Override
+    public double getS() {
+        return 0;
+    }
+
+    @Override
+    public void setS(double s) {
+
+    }
+
+    @Override
+    public void setAlasPrisma(double alas) {
+
+    }
+
+    @Override
+    public double setkeliling(double keliling) {
+        return 0;
+    }
+}
